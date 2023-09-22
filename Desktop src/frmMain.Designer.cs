3d2f67ad131b5b962a8982b5982740ae6b969854
Grannaa <43929303+Grannaa@users.noreply.github.com>
2023-09-22 13:53:44 +0200

    +add desktop app executable and source code

---
+++ Desktop src/frmMain.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.lblNota = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(55, 124);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(90, 23);
+            this.btnGuardar.TabIndex = 0;
+            this.btnGuardar.Text = "Guardar Nota";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(151, 124);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(75, 23);
+            this.btnLeer.TabIndex = 1;
+            this.btnLeer.Text = "Leer Nota";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(232, 124);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(75, 23);
+            this.btnBorrar.TabIndex = 2;
+            this.btnBorrar.Text = "Borrar Nota";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.AccessibleName = "Insertar texto";
+            this.txtResult.Location = new System.Drawing.Point(45, 12);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(100, 20);
+            this.txtResult.TabIndex = 3;
+            this.txtResult.Text = "Insertar texto";
+            // 
+            // lblNota
+            // 
+            this.lblNota.AutoSize = true;
+            this.lblNota.Location = new System.Drawing.Point(189, 15);
+            this.lblNota.Name = "lblNota";
+            this.lblNota.Size = new System.Drawing.Size(0, 13);
+            this.lblNota.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblNota);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnLeer);
+            this.Controls.Add(this.btnGuardar);
+            this.Name = "Form1";
+            this.Text = "Prueba Tecnica";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label lblNota;
+    }
+}
+
---
+++ Desktop src/frmMain.cs
@@ -0,0 +1,138 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmMain : Form
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private static readonly string url = "http://localhost:3000/";
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Utilizar el metodo POST
+                var values = new Dictionary<string, string>
+                {
+                    { "nota", this.txtResult.Text }
+                };
+                var content = new FormUrlEncodedContent(values);
+                var response = await client.PostAsync(url, content);
+                /* He escogido este switch porque cubre los dos errores mas comunes
+                 * una alternativa seria en lugar de crear un mensaje para cada tipo de error
+                 * crear un mensaje genérico de error para las respuestas que no sean las que quiera.
+                 * */
+                switch (response.StatusCode)
+                {
+                    case HttpStatusCode.NotFound:
+                        System.Windows.Forms.MessageBox.Show("Servicio no encontrado. Inténtelo más tarde.", "Error");
+                        break;
+                    case HttpStatusCode.InternalServerError:
+                        System.Windows.Forms.MessageBox.Show("Hubo un error con el servicio. Inténtelo más tarde.", "Error");
+                        break;
+                    default:
+                        var responseString = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine($"{responseString}");
+                        break;
+                }
+                
+            }
+            catch (HttpRequestException ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Servicio no disponible. Inténtelo más tarde.", "Error");
+            }
+            catch (WebException exc)
+            {
+                System.Windows.Forms.MessageBox.Show("Servicio no disponible. Inténtelo más tarde.", "Error");
+            }
+
+        }
+
+        private async void btnLeer_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Utilizar el metodo GET
+                var response = await client.GetAsync(url + "leer");
+                var responseString = await response.Content.ReadAsStringAsync();
+                switch (response.StatusCode)
+                {
+                    case HttpStatusCode.NotFound:
+                        System.Windows.Forms.MessageBox.Show("Servicio no encontrado. Inténtelo más tarde.", "Error");
+                        break;
+                    case HttpStatusCode.InternalServerError:
+                        System.Windows.Forms.MessageBox.Show("Hubo un error con el servicio. Inténtelo más tarde.", "Error");
+                        break;
+                    default:
+                        Console.WriteLine($"{responseString}");
+                        //Esta linea es para que el string lo lea como un JSON
+                        this.lblNota.Text = desglosarLectura(JsonConvert.DeserializeObject<Dictionary<string, string>>(responseString));
+                        break;
+                }
+                
+            }
+            catch (HttpRequestException ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Servicio no disponible. Inténtelo más tarde.", "Error");
+            }
+
+        }
+
+        private async void btnBorrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var response = await client.DeleteAsync(url + "delete");
+                switch (response.StatusCode)
+                {
+                    case HttpStatusCode.NotFound:
+                        System.Windows.Forms.MessageBox.Show("Servicio no encontrado. Inténtelo más tarde.", "Error");
+                        break;
+                    case HttpStatusCode.InternalServerError:
+                        System.Windows.Forms.MessageBox.Show("Hubo un error con el servicio. Inténtelo más tarde.", "Error");
+                        break;
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Servicio no disponible. Inténtelo más tarde.", "Error");
+            }
+
+        }
+
+        private string desglosarLectura(Dictionary<string, string> notas)
+        {
+            //Esta funcion sirve para transformar el JSON string a un formato mas estetico: Nota n : esta es la nota n
+            string notaDesglosada = "";
+            int cont = 0;
+
+            while (notas.Count >= cont)
+            {
+                string numNota = "Nota " + cont;
+                if (notas.ContainsKey(numNota))
+                {
+                    notaDesglosada += numNota + ": " + notas[numNota] + "\n";
+                }
+                
+                cont++;
+            }
+
+            return notaDesglosada;
+        }
+    }
+}
